be60f5fe73cea33e901ce4fe9150761497234869
hoyoung <skghdud024@naver.com>
2024-11-10 01:08:12 +0900

    수정

--- webproject/project.js
+++ webproject/project.js
@@ -14,3 +14,36 @@ function btn() {
     flag = !flag
     console.log(flag)
 }
+
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+function melonCrawler() {
+  const URL = `https://www.melon.com/chart/`;
+
+  axios.get(URL).then(res => {
+    console.log(res.status)
+    if(res.status == 200) {
+
+      // empty array
+      let crawledMusic= [];
+
+      // res.data에 있는 tag를 cheerio로 검색하여 변수에 담기
+      const $ = cheerio.load(res.data);
+      const $musicList = $('#lst50');
+
+      $musicList.each(function(i) {
+        crawledMusic[i] ={
+          title : $(this).find('#lst50 > td > div > div > div.ellipsis.rank01 > span > a').text().trim(),
+          artist : $(this).find('#lst50 > td > div > div > div.ellipsis.rank02 > a').text(),
+          img : $(this).find('#lst50 > td > div > a > img').attr('src')
+        };
+      });
+      console.log(crawledMusic)
+    } else {
+      console.log("server response error")
+    }
+  })
+}
+
+melonCrawler()
