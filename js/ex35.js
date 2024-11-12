edd3c6c7e16e51e25241c908d9637d57eeea8e4a
hoyoung <skghdud024@naver.com>
2024-11-12 17:43:01 +0900

    수업끝

---
+++ js/ex35.js
@@ -0,0 +1,13 @@
+const obj = {
+    t01 : () =>{
+        console.log("t01실행")
+    },
+    name : "홍길동"
+}
+const test01 = () => {
+    console.log("test01")
+}
+
+function test02(){
+    console.log("test02")
+}
