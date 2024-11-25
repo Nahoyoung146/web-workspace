65fa39958c75ede7d0331d927cb6933ea3ac4b7d
hoyoung <skghdud024@naver.com>
2024-11-25 23:38:35 +0900

    수정

--- nodeproject/main.js
+++ nodeproject/main.js
@@ -4,11 +4,20 @@ const app = express()
 app.set("views", "./views")
 app.set("view engine", "ejs")
 
-const main = (req,res) => {
+const main = (req, res) => {
     res.render("project")
 }
 
+const game = (req, res) => {
+    res.render("game")
+}
+
+const outline = (req, res) => {
+    res.render("outline")
+}
 app.get("/", main)
+app.get("/game", game)
+app.get("/outline", outline)
 
 app.listen(5000, () => {
     console.log("port start")
